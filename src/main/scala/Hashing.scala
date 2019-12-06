820329757d2da0b83ebc4c97fc29cf0bb0dfb1a3
Saurabh Vijay Singh <ssing60@uic.edu>
2019-12-05 19:35:19 -0600

    Hash Implementation to Decimal!

--- src/main/scala/Hashing.scala
+++ src/main/scala/Hashing.scala
@@ -1,17 +1,26 @@
 import java.security.MessageDigest
-
 import com.akka.utils.HashUtils
 
 object Hashing {
   def main(args: Array[String]): Unit = {
+    val id = 1
+    val md = java.security.MessageDigest.getInstance("SHA-1")
+    val hash = md.digest(id.toString.getBytes("UTF-8")).map("%02x".format(_)).mkString
+    val bin = BigInt(hash, 16).toString(2).take(3)
+    val hashed_id = Integer.parseInt(bin, 2)
+    println(hashed_id)
 
-    //val md = java.security.MessageDigest.getInstance("SHA-1").digest("Saurabh".getBytes("UTF-8")).map("%02x".format(_)).mkString
-    /*println(md.digest("Saurabh".getBytes("UTF-8")).map("%02x".format(_)).mkString)
+
+
+
+
+
+
+
+    /*println(md.digest("Vijay".getBytes("UTF-8")).map("%02x".format(_)).mkString)
     println(md.digest("Vijay".getBytes("UTF-8")).map("%02x".format(_)).mkString)
-    println(md.digest("Vijay".getBytes("UTF-8")).map("%02x".format(_)).mkString)
-    println(md.digest("Singh".getBytes("UTF-8")).map("%02x".format(_)).mkString)
-*/
-    println(HashUtils.generateHash("2", 5, "SHA-1"))
+    println(md.digest("Singh".getBytes("UTF-8")).map("%02x".format(_)).mkString)*/
+    //println(HashUtils.generateHash("2", 5, "SHA-1"))
 
     /*val hash = getHash("Saurabh",5)
     println(hash)*/
--- src/main/scala/com/akka/utils/HashUtils.scala
+++ src/main/scala/com/akka/utils/HashUtils.scala
@@ -3,6 +3,9 @@ package com.akka.utils
 object HashUtils {
   def generateHash(uniqueId: String, numBits: Int, hashingAlgorithm: String) = {
     val md = java.security.MessageDigest.getInstance(hashingAlgorithm).digest(uniqueId.getBytes("UTF-8")).map("%02x".format(_)).mkString
-    md.substring(0, numBits)
+    val bin = BigInt(md, 16).toString(2).take(numBits)
+    val hashed_id = Integer.parseInt(bin, 2)
+    hashed_id
+    //md.substring(0, numBits)
   }
 }
